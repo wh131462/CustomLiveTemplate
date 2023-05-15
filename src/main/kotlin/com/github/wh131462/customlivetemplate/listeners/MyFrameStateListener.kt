9943730312c347ec248c4b50a8f61871df97cd9a
GitHub Action <action@github.com>
2023-05-15 04:30:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/wh131462/customlivetemplate/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/wh131462/customlivetemplate/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wh131462.customlivetemplate.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/wh131462/customlivetemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wh131462/customlivetemplate/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wh131462.customlivetemplate.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wh131462.customlivetemplate.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
